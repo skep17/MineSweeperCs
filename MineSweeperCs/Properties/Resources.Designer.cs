0bf51dd0079977fdbef72227601e18c021c8cbf0
skep17 <kuprashvili.khvicha@gmail.com>
2025-06-04 15:23:48 +0200

    Added application icon.

--- MineSweeperCs/Properties/Resources.Designer.cs
+++ MineSweeperCs/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace MineSweeperCs.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon MineSweeperCsIcon {
+            get {
+                object obj = ResourceManager.GetObject("MineSweeperCsIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
